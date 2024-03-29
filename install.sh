83882247cdb2362ea636f10524661f603637a506
mrpep <leonardodpepino@gmail.com>
2024-03-29 21:03:31 +0000

    install encodecmae

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
+git submodule add https://github.com/nttcslab/byol-a.git models/byola
 curl -O https://raw.githubusercontent.com/lucidrains/byol-pytorch/2aa84ee18fafecaf35637da4657f92619e83876d/byol_pytorch/byol_pytorch.py
 patch < models/byola/byol_a/byol_pytorch.diff
 mv byol_pytorch.py models/byola/byol_a
 curl -O https://raw.githubusercontent.com/daisukelab/general-learning/7b31d31637d73e1a74aec3930793bd5175b64126/MLP/torch_mlp_clf.py
-mv torch_mlp_clf.py utils
+mv torch_mlp_clf.py models/byola/utils
+
+cd models/encodecmae && pip install -e .
